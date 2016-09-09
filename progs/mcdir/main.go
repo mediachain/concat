974cd796d22595a39b3f56dad18471960d5fb84c
vyzo <vyzo@hackzen.org>
2016-09-09 12:56:49 +0300

    mcdir: a bit of refactoring

--- progs/mcdir/main.go
+++ progs/mcdir/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"context"
+	"flag"
+	"fmt"
 	ggio "github.com/gogo/protobuf/io"
 	p2p_crypto "github.com/ipfs/go-libp2p-crypto"
 	p2p_peer "github.com/ipfs/go-libp2p-peer"
 	p2p_pstore "github.com/ipfs/go-libp2p-peerstore"
 	multiaddr "github.com/jbenet/go-multiaddr"
 	p2p_host "github.com/libp2p/go-libp2p/p2p/host"
-	p2p_bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
-	p2p_metrics "github.com/libp2p/go-libp2p/p2p/metrics"
 	p2p_net "github.com/libp2p/go-libp2p/p2p/net"
-	p2p_swarm "github.com/libp2p/go-libp2p/p2p/net/swarm"
+	mc "github.com/mediachain/concat/mc"
 	pb "github.com/mediachain/concat/proto"
 	"log"
 	"sync"
@@ -103,8 +102,11 @@ func (dir *Directory) unregisterPeer(pid p2p_peer.ID) {
 }
 
 func main() {
+	port := flag.Int("l", 9000, "Listen port")
+	flag.Parse()
+
 	log.Printf("Generating key pair\n")
-	privk, pubk, err := p2p_crypto.GenerateKeyPair(p2p_crypto.RSA, 1024)
+	privk, pubk, err := mc.GenerateKeyPair()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -115,27 +117,16 @@ func main() {
 	}
 	log.Printf("ID: %s\n", id.Pretty())
 
-	pstore := p2p_pstore.NewPeerstore()
-	pstore.AddPrivKey(id, privk)
-	pstore.AddPubKey(id, pubk)
-
-	addr, err := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/9000")
+	addr, err := mc.ParseAddress(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", *port))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	netw, err := p2p_swarm.NewNetwork(
-		context.Background(),
-		[]multiaddr.Multiaddr{addr},
-		id,
-		pstore,
-		p2p_metrics.NewBandwidthCounter())
-
+	host, err := mc.NewHost(privk, addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	host := p2p_bhost.New(netw)
 	dir := &Directory{pkey: privk, id: id, host: host}
 	host.SetStreamHandler("/mediachain/dir/register", dir.registerHandler)
 	host.SetStreamHandler("/mediachain/dir/lookup", dir.lookupHandler)
