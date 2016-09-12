f8cd3902cfb375972f4e1c11dcd4b8cd936a4c29
vyzo <vyzo@hackzen.org>
2016-09-12 18:48:41 +0300

    add setup script for installing dependencies

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+die() {
+    echo "FAILED"
+    exit 1
+}
+
+echo "Installing gx"
+go get github.com/whyrusleeping/gx github.com/whyrusleeping/gx-go || die
+
+echo "Installing libp2p deps"
+gx --verbose install  || die
+
+echo "Installing unvendored deps"
+go get github.com/gorilla/mux || die
+
+echo "DONE"
+
