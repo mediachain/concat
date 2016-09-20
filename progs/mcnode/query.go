5cad55585d3923fc2a2a6ddddcd6be41ef78edcd
vyzo <vyzo@hackzen.org>
2016-09-20 21:17:46 +0300

    query eval: criteria filters

--- progs/mcnode/query.go
+++ progs/mcnode/query.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	pb "github.com/mediachain/concat/proto"
+	"log"
 	"strconv"
 )
 
@@ -185,25 +186,144 @@ func ParseQuery(qs string) (*Query, error) {
 	return ps.query, nil
 }
 
-// query evaluation: very primitive eval, until we have an index and we
-// can compile to sql.
+// query evaluation: very primitive eval with only simple statements
+// will have to do until we have an index and we can compile to sql.
 func EvalQuery(query *Query, stmts []*pb.Statement) []interface{} {
-	qe := prepareEval(query)
-	qe.begin(len(stmts))
+	filter := makeCriteriaFilter(query)
+	rs := makeResultSet(query)
+
+	rs.begin(len(stmts))
 	for _, stmt := range stmts {
-		qe.eval(stmt)
+		if filter(stmt) {
+			rs.add(stmt)
+		}
 	}
-	qe.end()
-	return qe.result()
+	rs.end()
+
+	return rs.result()
 }
 
-type QueryEval interface {
+type QueryResultSet interface {
 	begin(hint int)
-	eval(*pb.Statement)
+	add(*pb.Statement)
 	end()
 	result() []interface{}
 }
 
-func prepareEval(query *Query) QueryEval {
+type CriteriaFilter func(*pb.Statement) bool
+type ValueCriteriaFilter func(*pb.Statement, string) bool
+type TimeCriteriaFilter func(*pb.Statement, int64) bool
+type CompoundCriteriaFilter func(stmt *pb.Statement, left, right CriteriaFilter) bool
+
+func idCriteriaFilter(stmt *pb.Statement, id string) bool {
+	return stmt.Id == id
+}
+
+func publisherCriteriaFilter(stmt *pb.Statement, pub string) bool {
+	return stmt.Publisher == pub
+}
+
+func sourceCriteriaFilter(stmt *pb.Statement, src string) bool {
+	// only support simple statements for now, so src = publisher
+	return stmt.Publisher == src
+}
+
+var valueCriteriaFilters = map[string]ValueCriteriaFilter{
+	"id":        idCriteriaFilter,
+	"publisher": publisherCriteriaFilter,
+	"source":    sourceCriteriaFilter}
+
+func timestampFilterLTEQ(stmt *pb.Statement, ts int64) bool {
+	return stmt.Timestamp <= ts
+}
+
+func timestampFilterLT(stmt *pb.Statement, ts int64) bool {
+	return stmt.Timestamp < ts
+}
+
+func timestampFilterEQ(stmt *pb.Statement, ts int64) bool {
+	return stmt.Timestamp == ts
+}
+
+func timestampFilterGTEQ(stmt *pb.Statement, ts int64) bool {
+	return stmt.Timestamp >= ts
+}
+
+func timestampFilterGT(stmt *pb.Statement, ts int64) bool {
+	return stmt.Timestamp >= ts
+}
+
+var timeCriteriaFilters = map[string]TimeCriteriaFilter{
+	"<=": timestampFilterLTEQ,
+	"<":  timestampFilterLT,
+	"=":  timestampFilterEQ,
+	">=": timestampFilterGTEQ,
+	">":  timestampFilterGT}
+
+func compoundCriteriaAND(stmt *pb.Statement, left, right CriteriaFilter) bool {
+	return left(stmt) && right(stmt)
+}
+
+func compoundCriteriaOR(stmt *pb.Statement, left, right CriteriaFilter) bool {
+	return left(stmt) || right(stmt)
+}
+
+var compoundCriteriaFilters = map[string]CompoundCriteriaFilter{
+	"AND": compoundCriteriaAND,
+	"OR":  compoundCriteriaOR}
+
+func makeCriteriaFilter(query *Query) CriteriaFilter {
+	c := query.criteria
+	if c == nil {
+		return func(*pb.Statement) bool { return true }
+	}
+
+	return makeCriteriaFilterF(c)
+}
+
+func makeCriteriaFilterF(c QueryCriteria) CriteriaFilter {
+	switch c := c.(type) {
+	case *ValueCriteria:
+		filter, ok := valueCriteriaFilters[c.sel]
+		if ok {
+			return func(stmt *pb.Statement) bool {
+				return filter(stmt, c.val)
+			}
+		} else {
+			log.Fatalf("Unexpected criteria selector: %s", c.sel)
+			return nil
+		}
+
+	case *TimeCriteria:
+		filter, ok := timeCriteriaFilters[c.op]
+		if ok {
+			return func(stmt *pb.Statement) bool {
+				return filter(stmt, c.ts)
+			}
+		} else {
+			log.Fatalf("Unexpected criteria time op: %s", c.op)
+			return nil
+		}
+
+	case *CompoundCriteria:
+		filter, ok := compoundCriteriaFilters[c.op]
+		if ok {
+			left := makeCriteriaFilterF(c.left)
+			right := makeCriteriaFilterF(c.right)
+			return func(stmt *pb.Statement) bool {
+				return filter(stmt, left, right)
+			}
+		} else {
+			log.Fatalf("Unexpected criteria combinator: %s", c.op)
+			return nil
+		}
+
+	default:
+		log.Fatalf("Unexpected criteria type: %t", c)
+		return nil
+	}
+}
+
+func makeResultSet(query *Query) QueryResultSet {
 	return nil
 }
