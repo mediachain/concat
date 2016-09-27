fb5a9699be719bffad73002f884dabf908aea672
vyzo <vyzo@hackzen.org>
2016-09-27 10:21:55 +0300

    query: remove RowSelector from interface, keep it simple for now.

--- mc/query/compile.go
+++ mc/query/compile.go
@@ -4,23 +4,20 @@ import (
 	"errors"
 )
 
-// Constructor for row selectors; individual RowSelectors should only
-// be used in a single threaded context.
-type MakeRowSelector func() RowSelector
-
 // A RowSelector extracts the next value from an sql result set
 type RowSelector interface {
 	Scan(src RowScanner) (interface{}, error)
 }
 
-// common interface between sql.Row and sql.Rows
+// Common Scan interface between sql.Row and sql.Rows
 type RowScanner interface {
 	Scan(res ...interface{}) error
 }
 
 // CompileQuery compiles a query to sql.
-// Returns the compiled sql query and a selector constructor for extracting
-// values from an sql.Rows result set (or a single Row)
-func CompileQuery(q *Query) (string, MakeRowSelector, error) {
+// Returns the compiled sql query and a selector for extracting
+// values from an sql result set
+// Note: The row selector should be used in single-threaded context
+func CompileQuery(q *Query) (string, RowSelector, error) {
 	return "", nil, errors.New("CompileQuery: Implement Me!")
 }
