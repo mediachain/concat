93c74d7a78e9219cfe8d2eba5a60d8dc53ac65b4
vyzo <vyzo@hackzen.org>
2016-10-17 20:20:33 +0300

    query/compile: wki/secondary index compilation

--- mc/query/compile.go
+++ mc/query/compile.go
@@ -149,14 +149,34 @@ func compileQueryCriteria(q *Query, join bool) (string, error) {
 		return nscrit, nil
 	}
 
+	isindex := isIndexCriteria(q.criteria)
+	if isindex {
+		join = true
+	}
+
 	scrit, err := compileSelectorCriteria(q.criteria, join)
 	if err != nil {
 		return "", err
 	}
 
-	if nscrit != "" {
-		return fmt.Sprintf("%s AND %s", nscrit, scrit), nil
+	if isindex {
+		tabs, err := indexCriteriaTables(q.criteria)
+		if err != nil {
+			return "", err
+		}
+
+		src := "Envelope"
+		for _, tab := range tabs {
+			src = fmt.Sprintf("%s JOIN %s ON Envelope.id = %s.id", src, tab, tab)
+		}
+
+		scrit = fmt.Sprint("Envelope.id IN (SELECT Envelope.id FROM %s WHERE %s)", src, scrit)
 	}
+
+	if nscrit != "" {
+		scrit = fmt.Sprintf("%s AND %s", nscrit, scrit)
+	}
+
 	return scrit, nil
 }
 
@@ -196,6 +216,9 @@ func compileSelectorCriteria(c QueryCriteria, join bool) (string, error) {
 
 	case *RangeCriteria:
 		return fmt.Sprintf("%s %s %d", c.sel, c.op, c.val), nil
+
+	case *IndexCriteria:
+		return fmt.Sprintf("%s = '%s'", c.sel, c.val), nil
 
 	case *CompoundCriteria:
 		left, err := compileSelectorCriteria(c.left, join)
@@ -522,9 +545,6 @@ func isStatementCriteria(c QueryCriteria) bool {
 	case *ValueCriteria:
 		return c.sel == "id"
 
-	case *RangeCriteria:
-		return false
-
 	case *CompoundCriteria:
 		return isStatementCriteria(c.left) && isStatementCriteria(c.right)
 
@@ -535,3 +555,59 @@ func isStatementCriteria(c QueryCriteria) bool {
 		return false
 	}
 }
+
+func isIndexCriteria(c QueryCriteria) bool {
+	switch c := c.(type) {
+	case *IndexCriteria:
+		return true
+
+	case *CompoundCriteria:
+		return isIndexCriteria(c.left) || isIndexCriteria(c.right)
+
+	case *NegatedCriteria:
+		return isIndexCriteria(c.e)
+
+	default:
+		return false
+	}
+}
+
+func indexCriteriaTables(c QueryCriteria) (map[string]string, error) {
+	tabs := make(map[string]string)
+
+	err := collectIndexCriteriaTables(tabs, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return tabs, err
+}
+
+func collectIndexCriteriaTables(tabs map[string]string, c QueryCriteria) error {
+	switch c := c.(type) {
+	case *IndexCriteria:
+		tab, ok := indexCriteriaTableNames[c.sel]
+		if !ok {
+			return QueryCompileError(fmt.Sprintf("Unexpected index selector: %s", c.sel))
+		}
+
+		tabs[c.sel] = tab
+		return nil
+
+	case *CompoundCriteria:
+		err := collectIndexCriteriaTables(tabs, c.left)
+		if err != nil {
+			return err
+		}
+		return collectIndexCriteriaTables(tabs, c.right)
+
+	case *NegatedCriteria:
+		return collectIndexCriteriaTables(tabs, c.e)
+
+	default:
+		return nil
+	}
+}
+
+var indexCriteriaTableNames = map[string]string{
+	"wki": "References"}
